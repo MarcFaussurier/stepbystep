acba78d1d76ce74d92e1225c356f4c025db1ee7c
marcfsr <faussurier.marc@icloud.com>
2019-07-03 21:16:31 +0200

    better bundle.php

--- bundle.php
+++ bundle.php
@@ -6,21 +6,35 @@
  * Time: 18:43
  */
 
-$recursive_autoloader_detector = function($self, $path, $deep = 0) {
-    if ($deep > 10) {
-        throw new Exception("Unable to detect autoload.php... exiting");
-    }
-    if (file_exists("$path/vendor/autoload.php"))
-        return "$path/vendor/autoload.php";
-    else
-        return $self($self, "$path/../", ++$deep);
-};
-require_once $recursive_autoloader_detector($recursive_autoloader_detector, __DIR__ );
-if (isset($argv)) {
-    $GLOBALS["argv"] = $argv;
+/**
+ * @param $path
+ * @param int $deep
+ * @return string
+ * @throws \Exception
+ */
+if (!function_exists("recursive_autoloader_detector")) {
+    function recursive_autoloader_detector($path, $deep = 0) {
+        if ($deep > 10)
+            throw new Exception("Unable to detect autoload.php... exiting");
+        else if (file_exists("$path/vendor/autoload.php"))
+            return "$path/vendor/autoload.php";
+        else
+            return recursive_autoloader_detector("$path/../", ++$deep);
+    };
 }
-if (isset($GLOBALS["main_bundle"])) {
-    return new \CloudsDotEarth\StepByStep\Bundle(__DIR__, $GLOBALS["main_bundle"]);
-} else {
-    return new \CloudsDotEarth\StepByStep\Bundle(__DIR__);
+try {
+    require_once recursive_autoloader_detector(__DIR__ );
+    if (isset($argv))
+        $GLOBALS["argv"] = $argv;
+    if (isset($GLOBALS["main_bundle"]))
+        return new \CloudsDotEarth\StepByStep\Bundle
+        (   __DIR__,
+            null,
+            null,
+            null  ,
+            $GLOBALS["main_bundle"]);
+    else
+        return new \CloudsDotEarth\StepByStep\Bundle(__DIR__);
+} catch (\Exception $exception) {
+    echo 'Caught exception: ',  $exception->getMessage(), "\n";
 }
